57a4bda1ce3ff371de3cfa5e08d78e003e75259c
mayahilbert <35238760+mayahilbert@users.noreply.github.com>
2025-10-02 11:43:29 -0400

    ios test

--- main-mask.js
+++ main-mask.js
@@ -246,7 +246,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     });
                 }
 
-                this.updateLetters();
+                //this.updateLetters();
                 this.updateImages();
             });
 
